2bf4ede59ab754f659f18d5bed60927c6ae064ef
shanyin liu <liu.shany@husky.neu.edu>
2020-05-16 21:00:11 -0400

    add

---
+++ precache-manifest.8f5b16908cd1382f6e40161149449508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865e528550e9243495bc2df0f2a1b582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ba9c975ba6d735963d",
+    "url": "/static/css/main.0cf487a0.chunk.css"
+  },
+  {
+    "revision": "7177a597e3ae81d1e306",
+    "url": "/static/js/2.6c2d45dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c2d45dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ba9c975ba6d735963d",
+    "url": "/static/js/main.b2e9bb07.chunk.js"
+  },
+  {
+    "revision": "44e40e03f23d4a63c0e5",
+    "url": "/static/js/runtime-main.05f58000.js"
+  }
+]);
